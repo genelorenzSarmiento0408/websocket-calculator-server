de45d781376b9c2665ad4a04621577450dd792af
Gene Lorenz Sarmiento <69145401+genelorenzSarmiento0408@users.noreply.github.com>
2022-07-09 14:32:08 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+from json import loads, dumps
+
+async def reducer(websocket):
+    async for message in websocket:
+        loaded_message = loads(message)
+        current_item = {
+            "currentValue": loaded_message["currentValue"],
+            "ok": True,
+            "err": None
+        }
+        try:
+            if loaded_message["type"] == "add":
+                current_item["currentValue"] += loaded_message["value"]
+            elif loaded_message["type"] == "minus":
+                current_item["currentValue"] -= loaded_message["value"]
+            else:
+                current_item["ok"] = False
+                current_item["err"] = "Invalid operation"
+        except KeyError:
+            current_item["ok"] = False
+            current_item["err"] = "Wrong object sent"
+        print(f"{current_item}")
+        await websocket.send(dumps(current_item))
+
+async def main():
+    async with websockets.serve(reducer, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
